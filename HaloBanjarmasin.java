2e8e1940a04cda151882bae2f1db2154ffc51897
Ahmad Fikri Hidayat Al-Islami <fikriihidayat15@gmail.com>
2024-09-19 20:40:53 +0800

    Code-202

---
+++ HaloBanjarmasin.java
@@ -0,0 +1,8 @@
+package halo;
+
+public class HaloBanjarmasin {
+
+    public static void main(String[] args) {
+        System.out.println("Halo Banjarmasin");
+    }
+}
---
+++ Salam.java
@@ -0,0 +1,12 @@
+package halo;
+
+public class Salam {
+
+    public static void main(String[] args) {
+        String negara = "Indonesia";
+        long jmlProv = 200000000;
+
+        System.out.println("Halo " + negara);
+        System.out.println("Jumlah provinsi anda adalah " + jmlProv);
+    }
+}
